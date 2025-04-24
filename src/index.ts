43897130f22c1d90d555ced69802b9dffed036b1
omurilolima <muriloserafimlima@gmail.com>
2025-04-24 13:17:46 +0100

    Enums

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-let age: number = 20;
-if (age < 50) age += 10;
-console.log(age);
+enum Size {
+	Small = 1,
+	Medium,
+	Large,
+}
+
+// Enum uses PascalCase
+// If you define an enum using the "const" keyword, the compiler will
+// generate more optimized code.
+let mySize: Size = Size.Medium;
+console.log(mySize);
