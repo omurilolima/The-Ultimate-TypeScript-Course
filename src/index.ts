d6265d05694f6d61b0804edcd137447f0195dbed
omurilolima <msl@cesar.school>
2025-04-24 13:40:56 +0100

    Functions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-enum Size {
-	Small = 1,
-	Medium,
-	Large,
+// As a best practice, we should always properly annotate our functions
+// For a better experience, in the tsconfig.json uncomment
+// noUnusedParameters , noUnusedLocals and noImplicitReturns
+
+// If you supplie a default value for a parameter, it will be optional when calling that function
+function calculateTax(income: number, taxYear = 2022): number {
+	if (taxYear < 2022) return income * 1.2;
+	return income * 1.3;
 }
 
-// Enum uses PascalCase
-// If you define an enum using the "const" keyword, the compiler will
-// generate more optimized code.
-let mySize: Size = Size.Medium;
-console.log(mySize);
+// If you pass a value for an optional parameter, it will override the default value
+calculateTax(10_000, 2023);
