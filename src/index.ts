a3c85fd13d11e19698ecb8c68fb440410e44cfb1
omurilolima <msl@cesar.school>
2025-04-25 11:30:45 +0100

    Nullish Coalescing operator

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
-type Customer = {
-	birthday?: Date;
+let speed: number | null = null;
+let ride = {
+	// Falsy (undefined, null, '', false, 0)
+	// example: speed: speed || 30
+	// Nullish Coalescing operator (??) is used when
+	// the values can be null or undefined.
+	speed: speed ?? 30,
 };
-
-function getCustomer(id: number): Customer | null {
-	return id === 0 ? null : { birthday: new Date() };
-}
-
-let customer = getCustomer(1);
-// Optional property operator "?"
-console.log(customer?.birthday?.getFullYear());
-
-// Optional element access operator
-//  customer?.[0]
-
-// Optional call
-let log: any = null;
-log?.("a");
