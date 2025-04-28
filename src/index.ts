f8f31881e8a8514de487e462db19e3008ab51e1b
omurilolima <msl@cesar.school>
2025-04-28 15:18:43 +0100

    Access Control Keywords

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,31 @@
 class Account {
-	// properties
+	// When we declare properties, all the properties are public by default
+	// so, we dont have to use the public key word right here.
 	readonly id: number; // read-only property
 	owner: string;
-	balance: number;
+	// When we use the PRIVATE keyword, it becomes only accessible within the class.
+	// By convention we start the private property name with an undercore
+	private _balance: number;
 	nickname?: string; // optional property
 
-	// constructor
+	// CONSTRUCTOR
 	constructor(id: number, owner: string, balance: number) {
 		this.id = id;
 		this.owner = owner;
-		this.balance = balance;
+		this._balance = balance;
 	}
-	// methods
+	// METHODS
+	// Private keyword can also be used to create methods that are not accessible from the outside
 	deposit(amount: number): void {
 		if (amount <= 0) throw new Error("Invalid amount");
-		this.balance += amount;
+		this._balance += amount;
+	}
+	// If you want to display a private property value to the user
+	// you need to create a method like this:
+	getBalance(): number {
+		return this._balance;
 	}
 }
 
 let account = new Account(1, "Mosh", 0);
-account.deposit(100);
-console.log(account.balance);
-console.log(account instanceof Account);
+console.log(account.getBalance());
