a472f1a5b41f257edfd9eec64b5f2a4e41dd7a3f
omurilolima <msl@cesar.school>
2025-04-28 15:42:51 +0100

    Getters and Setters

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ class Account {
 	nickname?: string; // optional property
 
 	// CONSTRUCTOR
+
+	// when we declate a parameter, we can prefix it with an access modifier
+	// like PUBLIC or PRIVATE, and this tells the compiler to create a propertu by this name
+	// and initialize it in one go. We can also use the READONLY keyword.
+
 	constructor(
 		public readonly id: number,
 		public owner: string,
@@ -12,10 +17,11 @@ class Account {
 		if (amount <= 0) throw new Error("Invalid amount");
 		this._balance += amount;
 	}
-	getBalance(): number {
+
+	get balance(): number {
 		return this._balance;
 	}
 }
 
 let account = new Account(1, "Mosh", 0);
-console.log(account.getBalance());
+console.log(account.balance);
