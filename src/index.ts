9184c36b1d4c83d504e55fec8ed33567f05636bf
omurilolima <msl@cesar.school>
2025-04-28 15:48:51 +0100

    Index Signatures

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,9 @@
-class Account {
-	nickname?: string; // optional property
-
-	// CONSTRUCTOR
-
-	// when we declate a parameter, we can prefix it with an access modifier
-	// like PUBLIC or PRIVATE, and this tells the compiler to create a propertu by this name
-	// and initialize it in one go. We can also use the READONLY keyword.
-
-	constructor(
-		public readonly id: number,
-		public owner: string,
-		private _balance: number
-	) {}
-	// METHODS
-	deposit(amount: number): void {
-		if (amount <= 0) throw new Error("Invalid amount");
-		this._balance += amount;
-	}
-
-	get balance(): number {
-		return this._balance;
-	}
+class SeatAssignment {
+	// A1, A2, ...
+	// Indes signature property
+	[searNumber: string]: string;
 }
 
-let account = new Account(1, "Mosh", 0);
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = "Mosh";
+seats.A2 = "Murilo";
