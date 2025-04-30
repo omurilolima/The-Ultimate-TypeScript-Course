c3f4568c0c7790ebaed90f2da546f259dcd0f273
omurilolima <msl@cesar.school>
2025-04-30 18:57:33 +0100

    Generic Intergaces

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-class ArrayUtils {
-	static wrapInArray<T>(value: T) {
-		return [value];
-	}
+//  http://mywebsite.com/users
+//  http://mywebsite.com/products
+
+// Create an Interface to store the result of calling an API endpoint
+// T | null is used because we may not necessarily get the data
+// T | null is used because we may not necessarily get an error
+
+interface Result<T> {
+	data: T | null;
+	error: string | null;
 }
 
-let numbers = ArrayUtils.wrapInArray(1);
+function fetch<T>(url: string): Result<T> {
+	return { data: null, error: null };
+}
+
+interface User {
+	username: string;
+}
+
+interface Product {
+	title: string;
+}
+
+let result = fetch<Product>("url");
+result.data?.title;
+
+let result2 = fetch<User>("url");
+result2.data?.username;
