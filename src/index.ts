b0b91dac0ccc6617d9b1373f851d6525612f2ab3
omurilolima <msl@cesar.school>
2025-04-30 16:28:23 +0100

    Interfaces Vs Types

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,21 @@
-// If a the calendar class is not providing any logic or algorithm
-// that subclasses can reuse.  So, in this case, it's better to use an interface.
-// because the code will end up being more concise and shorter both in
-// Typescript and Javascript
+// Interfaces vs Types
+// In TypeScript, interfaces and type aliases can be used interchangeably.
+// Both can be used to describe the shape of an object:
 
-interface Calendar {
+// INTERFACE
+interface Person {
 	name: string;
-	addEvent(): void;
-	removeEvent(): void;
 }
-// In constrast, if you had some logic, algorithm or method with a few lines
-// and you wanted to share that code amongst subclasses, then you couldn't use
-// an interface because they can not have methods impementations.
+let person: Person = {
+	name: "Mosh",
+};
 
-// abstract class Calendar {
-// 	constructor(public name: string) {}
-// 	abstract addEvent(): void;
-// 	abstract removeEvent(): void;
-// }
+// TYPE;
+type PersonT = {
+	name: string;
+};
+let persont: Person = {
+	name: "Mosh",
+};
 
-// Interfaces can be extended using the "implements" keyword
-
-class GoogleCalendar implements Calendar {
-	constructor(public name: string) {}
-
-	addEvent(): void {
-		throw new Error("Method not implemented.");
-	}
-	removeEvent(): void {
-		throw new Error("Method not implemented.");
-	}
-}
+// It’s more conventional to use an interface in front of the extends keyword, though.
