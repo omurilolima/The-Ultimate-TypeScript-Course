ee54f9d55d3bbf207916fcc2e2115e78741d68a4
omurilolima <msl@cesar.school>
2025-05-01 18:53:51 +0100

    Type Mapping

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,42 @@
+// TYPE MAPPING
+//  Sometimes, we need to base a type on another type.
+// This is called type mapping. For exemple, here we have
+// the product interface with two properties.
+
 interface Product {
 	name: string;
 	price: number;
 }
 
-// Defining a Generic Classes
+// What if somewhere else in our application, we need a product with
+// Read Only properties?
 
-class Store<T> {
-	// Here we don't need a constructor because we are initializing (to an empty array)
-	// We are defining it as protected to prevent objects of beeing reassigned out of a class
-	// and inherited in child classes
-	protected _objects: T[] = [];
+// We start with type keyword. Because here we have to create a type alias.
+// So we cannot use an interface.
 
-	add(obj: T): void {
-		this._objects.push(obj);
-	}
+type ReadOnly<T> = {
+	// Instead of hard coding these property names, we're going to use
+	// the Index signature syntax to dynamically add properties
+	// And using the keyof operator, we're going to dynamically get
+	// the properties of the properties of the product type.
+	// And make all of them read only in one go. This is what we call Type Mapping
+	readonly [K in keyof T]: T[K];
+};
 
-	// IF T is Product
-	// keyof T is gonna return 'name' | 'price'
-	find(property: keyof T, value: unknown): T | undefined {
-		return this._objects.find((obj) => obj[property] === value);
-	}
-}
+// Now we can create a ReadOnly Product or any other object
+let product: ReadOnly<Product> = {
+	name: "a",
+	price: 1,
+};
 
-let store = new Store<Product>();
-store.add({ name: "a", price: 1 });
-store.find("name", "a");
-store.find("price", "1");
+// Similarly we can create another Product type with OPTIONAL properties
+type Optional<T> = {
+	[K in keyof T]?: T[K];
+};
+
+// Similarly we can create another Product type with NULLABLE properties
+type Nullable<T> = {
+	[K in keyof T]: T[K] | null;
+};
+
+// TyperScript has many utility types like these. You can find more about it at https://www.typescriptlang.org/docs/handbook/utility-types.html
