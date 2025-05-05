027f4fffa725897641cb9f620715bd95cd0e9d76
omurilolima <msl@cesar.school>
2025-05-05 14:05:40 +0100

    Accessor Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,25 @@
-// To apply a decorator on a method, we need three parameters
-// 		1. The object that owns the target method.
-// 		2. The name of the target method
-// 		3. The descriptor object or the target method
-
-function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
-	// Here we can replace or enhance the original method
-	const original = descriptor.value as Function;
-
-	// The spread operator pass all the arguments to the original function
-	descriptor.value = function (...args: any) {
-		console.log("Before");
-		original.call(this, ...args); // Calling the original function
-		console.log("After");
+function Capitalize(
+	target: any,
+	methodName: string,
+	descriptor: PropertyDescriptor
+) {
+	// Get a reference to the original method
+	const original = descriptor.get;
+	// Redefine the getter
+	descriptor.get = function () {
+		const result = original?.call(this);
+		return typeof result === "string" ? result.toUpperCase() : result;
 	};
 }
 
 class Person {
-	@Log
-	say(message: string) {
-		console.log("Person says " + message);
+	constructor(public firstName: string, public lastName: string) {}
+
+	@Capitalize
+	get fullName() {
+		return `${this.firstName} ${this.lastName}`;
 	}
 }
 
-let person = new Person();
-person.say("Hello");
-
-// More about TypeScript Decorators at https://www.typescriptlang.org/docs/handbook/decorators.html
+let person = new Person("murilo", "lima");
+console.log(person.fullName);
